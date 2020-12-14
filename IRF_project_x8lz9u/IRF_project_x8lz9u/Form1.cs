cb0f959ac9d5a45a10473c24a5304310230bda4b
Kristóf Nagy <kristof.nagy2@stud.uni-corvinus.hu>
2020-12-14 19:22:47 +0100

    step1

--- IRF_project_x8lz9u/IRF_project_x8lz9u/Form1.cs
+++ IRF_project_x8lz9u/IRF_project_x8lz9u/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IRF_project_x8lz9u.valuataservice;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,11 @@ namespace IRF_project_x8lz9u
         public Form1()
         {
             InitializeComponent();
+
+            var valuta = new MNBArfolyamServiceSoapClient();
+          
+
+
         }
     }
 }
