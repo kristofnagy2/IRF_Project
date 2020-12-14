daf683d35365c824adb414e3babc77d08ea35beb
Kristóf Nagy <kristof.nagy2@stud.uni-corvinus.hu>
2020-12-14 19:25:22 +0100

    step3

---
+++ IRF_project_x8lz9u/IRF_project_x8lz9u/Entities/Money.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_project_x8lz9u.Entities
+{
+    class Money
+    {
+
+        public DateTime Date { get; set; }
+        public string Currency { get; set; }
+
+        public decimal Value { get; set; }
+    }
+}
