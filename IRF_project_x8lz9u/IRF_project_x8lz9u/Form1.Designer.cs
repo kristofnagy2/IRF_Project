1ecc92d848ee46270b4f1c2a4b1dd825b24138df
Kristóf Nagy <kristof.nagy2@stud.uni-corvinus.hu>
2020-12-14 19:41:34 +0100

    step5

--- IRF_project_x8lz9u/IRF_project_x8lz9u/Form1.Designer.cs
+++ IRF_project_x8lz9u/IRF_project_x8lz9u/Form1.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,11 +43,39 @@
             this.dataGridView1.Size = new System.Drawing.Size(446, 412);
             this.dataGridView1.TabIndex = 0;
             // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(503, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(503, 68);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 2;
+            this.dateTimePicker2.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(503, 124);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +87,9 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- IRF_project_x8lz9u/IRF_project_x8lz9u/Form1.cs
+++ IRF_project_x8lz9u/IRF_project_x8lz9u/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace IRF_project_x8lz9u
 {
@@ -22,21 +23,64 @@ namespace IRF_project_x8lz9u
         {
             InitializeComponent();
 
-            dataGridView1.DataSource = moneydata;
+            resfreshdata();
 
-            var valuta = new MNBArfolyamServiceSoapClient();
-
-            var inquiy = new GetExchangeRatesRequestBody()
+           void resfreshdata()
             {
-                currencyNames = "USD",
-                startDate = "2020-11-01",
-                endDate = "2020-12-03"
+                moneydata.Clear();
 
-            };
+                dataGridView1.DataSource = moneydata;
 
-            var feedback = valuta.GetExchangeRates(inquiy);
+                var valuta = new MNBArfolyamServiceSoapClient();
 
-            var end = feedback.GetExchangeRatesResult;
+                var inquiy = new GetExchangeRatesRequestBody()
+                {
+                    currencyNames = "USD",
+                    startDate = "2020-11-01",
+                    endDate = "2020-12-03"
+
+                };
+
+                var feedback = valuta.GetExchangeRates(inquiy);
+
+                var end = feedback.GetExchangeRatesResult;
+
+                var xml = new XmlDocument();
+                xml.LoadXml(end);
+
+                foreach (XmlElement item in xml.DocumentElement)
+                {
+                    var money = new Money();
+                    moneydata.Add(money);
+
+                    money.Date = DateTime.Parse(item.GetAttribute("date"));
+
+                    var childElement = (XmlElement)item.ChildNodes[0];
+                    money.Currency = childElement.GetAttribute("curr");
+
+                    var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                    var value = decimal.Parse(childElement.InnerText);
+                    if (unit != 0)
+                        money.Value = value / unit;
+                };
+
+            }
+        }
+            
+            
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            refreshdat
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
 
         }
     }
